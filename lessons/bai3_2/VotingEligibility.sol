339afd946cbd570e3fe2d23c2cbaec8d3ab49f33
ThuHoiBao <tranthu270904@gmail.com>
2025-12-26 00:51:46 +0700

    bai3_1 , bai3_2

---
+++ lessons/bai3_2/VotingEligibility.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.20;
+
+contract VotingEligibility {
+    uint public minAge = 18;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function checkEligibility(uint age) public view returns (bool) {
+        if (age >= minAge) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function updateMinAge(uint newMinAge) public {
+        require(msg.sender == owner, "Only owner can update minAge");
+        minAge = newMinAge;
+    }
+}
